ce068b3a2e7fecd7f4b66b7aae8cc3de79cb8ea0
Matej Holubec <matej.holubec@gmail.com>
2022-11-15 11:08:30 +0100

    feat: add login

--- src/router.ts
+++ src/router.ts
@@ -45,14 +45,21 @@ export default async (req: IncomingMessage, res: ServerResponse): Promise<any> =
 
         case "/enquiry/accept":
             if(!(parsed.id && parsed.userId)) return res.end("Need id and userId parameter - number")
-            const user = await User.fromId(parsed.userId);
-            const order = Order.acceptEnquiry(parsed.id, user)
+            const enquiringUser = await User.fromId(parsed.userId);
+            const order = Order.acceptEnquiry(parsed.id, enquiringUser)
             return res.end(JSON.stringify(order))
         case "/setNotificationToken":
             if(!(parsed.token && parsed.userId)) return res.end("Need token and userId - string;number");
             const result = await User.setNotificationToken(parsed.userId, parsed.token);
             return res.end(JSON.stringify(result))
-
+        case "/register":
+            if(!(parsed.email && parsed.name && parsed.password)) return res.end("Need email, name and password - strings");
+            const registeredUser = await User.register(parsed.email, parsed.name, parsed.password);
+            return res.end(JSON.stringify(registeredUser))
+        case "/login":
+            if(!(parsed.email && parsed.password)) return res.end("Need email and password - strings");
+            const loggedinUser = await User.login(parsed.email, parsed.password);
+            return res.end(JSON.stringify(loggedinUser))
 
         default:
             res.end("404 NOT FOUND")
